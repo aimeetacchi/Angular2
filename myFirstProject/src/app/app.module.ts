de987640603d810857a42f76acb935452153686f
Aimee T <aimeetacchi@hotmail.co.uk>
2017-10-30 21:51:29 +0000

    routes

--- myFirstProject/src/app/app.module.ts
+++ myFirstProject/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 // Components
 import { AppComponent } from './app.component';
@@ -12,6 +12,12 @@ import { HeroComponentComponent } from './hero-component/hero-component.componen
 import { FeaturedProductsComponentComponent } from './featured-products-component/featured-products-component.component';
 import { FooterComponentComponent } from './footer-component/footer-component.component';
 import { AboutComponentComponent } from './about-component/about-component.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+     {path: '', component: HomeComponent },
+    {path: 'about', component: AboutComponentComponent},
+]
 
 @NgModule({
   declarations: [
@@ -21,18 +27,14 @@ import { AboutComponentComponent } from './about-component/about-component.compo
     HeroComponentComponent,
     FeaturedProductsComponentComponent,
     FooterComponentComponent,
-    AboutComponentComponent
+    AboutComponentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot([
-    {
-      path: 'about',
-      component: AboutComponentComponent,
-    },
-      ]),
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
