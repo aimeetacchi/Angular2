32fca2e09297a8233bf92cd846227d551d086440
darkxangel84 <aimeetacchi@hotmail.co.uk>
2017-10-31 11:16:24 +0000

    set up routes

--- project2/mean-app/src/app/app.module.ts
+++ project2/mean-app/src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
+// Components
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+
+// Define the routes
+const ROUTES = [
+	{
+		path: '',
+		redirectTo: 'posts',
+		pathMatch: 'full'
+	},
+	{
+		path: 'posts',
+		component: PostsComponent
+	}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES) // Add routes to the app
   ],
   providers: [],
   bootstrap: [AppComponent]
