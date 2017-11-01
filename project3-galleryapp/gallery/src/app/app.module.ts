18c6e826de6a192954d4bb91b82d01b2de21095b
darkxangel84 <aimeetacchi@hotmail.co.uk>
2017-11-01 12:27:08 +0000

    project3-galleryapp

---
+++ project3-galleryapp/gallery/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+
+// Components
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageDetailComponent } from './image/image-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    GalleryComponent,
+    ImageDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AlertModule.forRoot()
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
